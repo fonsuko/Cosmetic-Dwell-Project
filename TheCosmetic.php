e9c5b4036c9ea7d35f010a15d77b0a0199df1afd
Chanida K <fonsukoth@gmail.com>
2017-06-27 14:24:33 +0700

    Update TheCosmetic.php

--- TheCosmetic.php
+++ TheCosmetic.php
@@ -1,4 +1,5 @@
 <!-- **design the graphic in AI and the coding it by add some CSS command**-->
+<!-- **pictures of cosmetic web are spare in web hosting**-->
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
